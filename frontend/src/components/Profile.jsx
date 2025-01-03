ba93c8fb0ae37e9b7e2c646c23c75fff48542a6d
CraziestAnimeLover <riyarsunny706@gmail.com>
2025-01-03 15:00:50 +0530

    profile update

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -103,14 +103,16 @@ const Profile = () => {
 
         {/* Skills Section */}
         <div className="my-5">
-          <h1>Skills</h1>
-          <div className="flex items-center gap-1">
-            {user?.profile?.skills.length !== 0
-              ? user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>)
-              : <span>NA</span>
-            }
-          </div>
-        </div>
+  <h1>Skills</h1>
+  <div className="flex items-center gap-1">
+    {/* Check if skills exists and is an array */}
+    {user?.profile?.skills && Array.isArray(user?.profile?.skills) && user?.profile?.skills.length > 0
+      ? user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>)
+      : <span>NA</span>
+    }
+  </div>
+</div>
+
 
         {/* Resume Section */}
         <div className="grid w-full max-w-sm items-center gap-1.5">
