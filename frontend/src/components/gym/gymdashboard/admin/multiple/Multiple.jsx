d12fd406d19576011cfc35d98f3c8347aab3db33
CraziestAnimeLover <riyarsunny706@gmail.com>
2025-03-08 15:21:01 +0530

    card

--- frontend/src/components/gym/gymdashboard/admin/multiple/Multiple.jsx
+++ frontend/src/components/gym/gymdashboard/admin/multiple/Multiple.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "../../../../ui/Card";
 import { Input } from "@/components/ui/input";
 
 const Multiple = () => {
